1e328b39f375cfeb715b9c4dcf1095a6b1032126
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-25 18:20:44 +0900

    Add initial files.

---
+++ DomainPrimitive/DomainPrimitive/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DomainPrimitive
+//
+//  Created by Yoshihiro Furukawa on 2023/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
