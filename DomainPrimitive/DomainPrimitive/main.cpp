83ff65022ae4a50ee8f1d8278c52dc79afce00b9
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-26 21:42:14 +0900

    ドメインプリミティブ確認用のコードを追加した

--- DomainPrimitive/DomainPrimitive/main.cpp
+++ DomainPrimitive/DomainPrimitive/main.cpp
@@ -7,11 +7,23 @@
 
 #include <iostream>
 #include "units/Distance.h"
+#include "CurrentAltitude.h"
 
 int main(int argc, const char * argv[]) {
-    auto m1 = Meter<long>{ 10 };
-    auto m2 = Meter<long>{ 20 };
+    auto m1 = Meter<double>{ 10 };
+    auto m2 = Meter<double>{ 20 };
+    auto m3 = Meter<double>{ 30 };
     std::cout << m1 + m2 << std::endl;
-    
+    std::cout << m1 - m2 << std::endl;
+    std::cout << m1 * m2 << std::endl;
+    std::cout << m1 / m2 << std::endl;
+    std::cout << m1 * 2.345 << std::endl;
+    std::cout << 2.234 * m1 << std::endl;
+    m1 *= 3.456;
+    std::cout << m1 << std::endl;
+
+    CurrentAltitude altitude(555);
+    std::cout << altitude.getAltitude() << std::endl;
     return EXIT_SUCCESS;
 }
+
