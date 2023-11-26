cd451e2f0fc02c11263c01910b64e491a594276f
Yoshihiro Furukawa <yoshi@YoshihironoMacBook-Pro.local>
2023-11-26 21:21:41 +0900

    デストラクタを忘れていたので追加した

--- DomainPrimitive/DomainPrimitive/CurrentAltitude.h
+++ DomainPrimitive/DomainPrimitive/CurrentAltitude.h
@@ -8,5 +8,20 @@
 #ifndef CurrentAltitude_h
 #define CurrentAltitude_h
 
+#include "units/Feet.h"
+
+class CurrentAltitude {
+private:
+    CurrentAltitude() = delete;
+public:
+    explicit CurrentAltitude( double altitude );
+    ~CurrentAltitude() = default;
+    
+    Feet<double> getAltitude();
+private:
+    double altitudeMin_;
+    double altitudeMax_;
+    Feet<double> altitude_;
+};
 
 #endif /* CurrentAltitude_h */
